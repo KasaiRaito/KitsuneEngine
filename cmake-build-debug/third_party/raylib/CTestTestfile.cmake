e0331f0d1ea8974ae28eac9604bde78e78b189f0
KasaiRaito <eduardo.huerta@saei.mx>
2026-03-18 16:27:27 -0600

    RemoveRedundancy

--- cmake-build-debug/third_party/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kasairaito/Documents/GitHub/KitsuneEngine/third_party/raylib
-# Build directory: /Users/kasairaito/Documents/GitHub/KitsuneEngine/cmake-build-debug/third_party/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
